b9e311879b8d5e02bd33c467a21af414b3e74f94
Kevin Masur <kevin.masur@icims.com>
2018-11-08 14:57:02 -0500

    whoops

--- src/main/java/com/icims/tony/school/repositories/CourseRepository.java
+++ src/main/java/com/icims/tony/school/repositories/CourseRepository.java
@@ -3,7 +3,6 @@ package com.icims.tony.school.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.icims.tony.demo.entities.DemoEntity;
 import com.icims.tony.school.entities.CourseEntity;
 
 @Repository
